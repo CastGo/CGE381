3412cedb1ae3b92b5937d2e29b3fbaf0ad7212de
CastGo <art.sskva@gmail.com>
2025-09-07 21:46:16 +0700

    1Enemy No Animation , Player Can Take Damage From Enemy

---
+++ My project/Assets/Scripts/EnemyAI_1.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI_1 : MonoBehaviour
+{
+    public float speed;
+    public float circleRadius;
+    private Rigidbody2D EnemyRB;
+    public GameObject groundCheck;
+    public LayerMask groundLayer;
+
+    public int damage = 1;
+
+    public int maxLife = 3;
+    private int currentLife;
+    private SpriteRenderer spriteRenderer;
+    private Color ogColor;
+
+    public bool facingRight;
+    public bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemyRB = GetComponent<Rigidbody2D>();
+        currentLife = maxLife;
+        //ogColor = spriteRenderer.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyRB.velocity = Vector2.right * speed * Time.deltaTime;
+        isGrounded = Physics2D.OverlapCircle(groundCheck.transform.position, circleRadius, groundLayer);
+        if(!isGrounded && facingRight)
+        {
+            Flip();
+        }
+        else if(!isGrounded && !facingRight)
+        {
+            Flip();
+        }
+
+        void Flip()
+        {
+            facingRight = !facingRight;
+            Vector3 Scaler = transform.localScale;
+            Scaler.x *= -1;
+            transform.localScale = Scaler;
+            speed *= -1;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundCheck.transform.position, circleRadius);
+    }
+    //private IEnumerator FlashWhite()
+    //{
+    //    spriteRenderer.color = Color.white;
+    //    yield return new WaitForSeconds(0.2f);
+    //    spriteRenderer.color = ogColor;
+    //}
+    public void TakeDamage(int damage)
+    {
+        currentLife -= damage;
+        //StartCoroutine(FlashWhite());
+        if (currentLife <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        //Go around loottable
+        //Spawn item
+        //foreach (LootItem lootItem in lootTable)
+        //{
+        //    if (Random.Range(0f, 100f) <= lootItem.dropChance)
+        //    {
+        //        InstantiateLoot(lootItem.itemPrefab);
+        //    }
+        //    break;
+        //}
+
+        Destroy(gameObject);
+    }
+
+    //void InstantiateLoot(GameObject loot)
+    //{
+    //    if (loot)
+    //    {
+    //        GameObject droppedLoot = Instantiate(loot, transform.position, Quaternion.identity);
+
+    //        droppedLoot.GetComponent<SpriteRenderer>().color = Color.red;
+    //    }
+    //}
+}
--- My project/Assets/Scripts/PlayerLife.cs
+++ My project/Assets/Scripts/PlayerLife.cs
@@ -14,26 +14,68 @@ public class PlayerLife : MonoBehaviour
 
     public static event Action OnPlayedDied;
 
+    [SerializeField] float damageInterval = 1f; // เวลาดีเลย์ต่อการโดนดาเมจแต่ละครั้ง
+    private float damageTimer = 0f;
     // Start is called before the first frame update
     void Start()
     {
         ResetHealth();
 
-        //spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         //GameController.OnReset += ResetHealth;
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
         {
-            Enemy enemy = collision.GetComponent<Enemy>();
-            if (enemy != null)
+            EnemyAI_1 enemyAI_1 = collision.GetComponent<EnemyAI_1>();
+            if (enemyAI_1)
             {
-                TakeDamage(enemy.damage);
+                TakeDamage(enemyAI_1.damage);
+            }
+            // รีเซ็ตเวลาใหม่ทันทีหลังโดน
+            damageTimer = 0f;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {        
+        if (collision.CompareTag("Enemy"))
+        {
+            damageTimer += Time.deltaTime;
+            if (damageTimer >= damageInterval)
+            {
+                EnemyAI_1 enemyAI_1 = collision.GetComponent<EnemyAI_1>();
+                if (enemyAI_1)
+                {
+                    TakeDamage(enemyAI_1.damage);
+                }
+                // รีเซ็ตเวลาเมื่อได้รับดาเมจ
+                damageTimer = 0f;
             }
         }
     }
+    
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Enemy"))
+    //    {
+    //        EnemyAI_1 enemyAI_1 = collision.GetComponent<EnemyAI_1>();
+    //        if (enemyAI_1)
+    //        {
+    //            TakeDamage(enemyAI_1.damage);
+    //        }
+    //    }
+    //    //Enemy enemy = collision.GetComponent<Enemy>();
+    //    //if (enemy)
+    //    //{
+    //    //    TakeDamage(enemy.damage);
+    //    //}
+    //    //EnemyAI_1 enemyAI_1 = collision.GetComponent<EnemyAI_1>();
+    //    //if (enemyAI_1)
+    //    //{
+    //    //    TakeDamage(enemyAI_1.damage);
+    //    //}
+    //}
 
     void ResetHealth()
     {
